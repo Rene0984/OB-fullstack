84017abd81e3c6d8fabf4a311bcd2fa2f1db8900
Rene0984 <106356604+Rene0984@users.noreply.github.com>
2022-06-16 17:40:41 -0500

    Add files via upload

---
+++ python/tema3/tema3.py
@@ -0,0 +1,21 @@
+nombre: str = 'rene'
+apellido: str = 'Garcia'
+edad: int = 37
+email: str = 'renegarcia0984@gmail.com'
+telefono: int = 823107000
+casado: bool = 'true'
+con_hijos: bool = 'true'
+con_hijos: bool = 'true'
+lista_de_amigos: list = {'amigo1', 'amigo2', 'amigo3'}
+peliculas_vistas = { 'pelicula 1': 'conjuro', 'peñicula 2': 'anabelle'}
+
+
+print (nombre)
+print(apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(con_hijos)
+print(lista_de_amigos)
+print(peliculas_vistas)
